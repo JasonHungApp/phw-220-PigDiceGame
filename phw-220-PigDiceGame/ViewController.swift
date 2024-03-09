c37afb1212e9a912569396287680b0d60c161202
jasonhung <hello@ifhavebugcallme.com>
2024-03-09 18:49:41 +0800

    Initial Commit

---
+++ phw-220-PigDiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  phw-220-PigDiceGame
+//
+//  Created by jasonhung on 2024/3/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
