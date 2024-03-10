3f01b072e66063e1c672061050222f59c544e1ea
jasonhung <hello@ifhavebugcallme.com>
2024-03-10 20:06:29 +0800

    v1

---
+++ phw-220-PigDiceGame/Dice.swift
@@ -0,0 +1,8 @@
+//
+//  Dice.swift
+//  phw-220-PigDiceGame
+//
+//  Created by jasonhung on 2024/3/9.
+//
+
+import Foundation
---
+++ phw-220-PigDiceGame/Player.swift
@@ -0,0 +1,8 @@
+//
+//  Player.swift
+//  phw-220-PigDiceGame
+//
+//  Created by jasonhung on 2024/3/10.
+//
+
+import Foundation
